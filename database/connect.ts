664ba256e4e898dd68be8bf3a66a1da7a2308132
Daniel Koller <daniel-koller@gmx.at>
2023-03-15 12:08:07 +0100

    Add deployment config and deploy

--- database/connect.ts
+++ database/connect.ts
@@ -1,8 +1,28 @@
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
 
-config();
+// This loads all environment variables from a .env file
+// for all code after this line
+if (!process.env.FLY_IO) config();
 
-export const sql = postgres({
-  transform: { ...postgres.camel, undefined: null },
-});
+// Type needed for the connection function below
+declare module globalThis {
+  let postgresSqlClient: ReturnType<typeof postgres> | undefined;
+}
+
+// Connect only once to the database
+// https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
+function connectOneTimeToDatabase() {
+  if (!globalThis.postgresSqlClient) {
+    globalThis.postgresSqlClient = postgres({
+      transform: {
+        ...postgres.camel,
+        undefined: null,
+      },
+    });
+  }
+  return globalThis.postgresSqlClient;
+}
+
+// Connect to PostgreSQL
+export const sql = connectOneTimeToDatabase();
